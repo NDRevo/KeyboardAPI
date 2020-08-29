db57fdd3ed9f373ef24c7b87a6287dcf874d5ef1
NDRevo <durann031@gmail.com>
2020-08-29 00:17:42 +0000

    Added website + domain

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const keebdataFiles = fs.readdirSync('./methods').filter(file => file.endsWith('
 var bodyParser = require('body-parser');
 var app = express();
 const router = express.Router();
+var path = require('path');
 
 //Sets favicon and json
 app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')));
@@ -23,18 +24,27 @@ app.use('/', router);
 app.listen(port); //Listening on port
 console.log("Keyboard API Online")
 
-
-//Redirects to readme 
 router.get('/', function (req, res) {
-    res.redirect('https://keyboardapi.readme.io/reference');
+    //Path.join because sendFile requires an absolute path and/or set root in config object
+    res.sendFile(path.join(__dirname + '/website/homepage.html'))
+ 
 });
 
 
+//Redirects to readme 
+router.get('/kbapi', function (req, res) {
+    res.redirect('https://keyboardapi.readme.io/reference');
+});
+router.get('/commissions', function (req, res) {
+    res.redirect('https://docs.google.com/forms/d/e/1FAIpQLScJGyq6MxgXIDrVbWpyUHE9CgbuhzAOAMlErIKI1cWptXwsbQ/viewform?usp=sf_link');
+});
+
 //Finds file and runs the file given the name of the data the user wants (Ex. Switches)
 router.use(function (req, res, next) {
-
-    var dataFile = keebdataFiles.find(data => data.includes(req.url.substr(1, 3)))
-
+    app.use('/kbapi', router);
+    var dataFile = keebdataFiles.find(data => data.includes(req.url.substr(7, 10)))
+    console.log(dataFile)
+    console.log(req.url.substr(7, 8))
     var runKeebFile = require("./methods/" + dataFile); //Gets the correct data file path 
     
     runKeebFile.execute(router); // Executes code in file and passes router
