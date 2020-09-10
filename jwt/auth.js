ee9fbb21e12f20c2dd607562568f1ea44f0c38de
Chris Wilsher <christopher.wilsher.2018@live.rhul.ac.uk>
2020-09-10 03:08:35 +0100

    converted auth to module

--- jwt/auth.js
+++ jwt/auth.js
@@ -4,39 +4,53 @@ const jwt = require('jsonwebtoken');
 var privateKEY = fs.readFileSync('./private.key', 'utf8');
 var publicKEY = fs.readFileSync('./public.key', 'utf8');
 
-// JWT SIGNING
+module.exports = {
+    // JWT SIGNING
+    sign: (payload, $Options) => {
+    /*
+    sOptions = {
+        i = 'Mechspace'; //Org issuing token
+        s = 'me@mechspace.co'; //User of Token
+        a = 'https://mechspace.co/'; //Intended Recipient of Token
+    }
+    */
+        var signOptions = {
+            issuer: $Options.i,
+            subject: $Options.s,
+            audience: $Options.a,
+            expiresIn: "12h",
+            algorithm: "RS256"
+        };
 
-// Payload
-var payload = {
-    data1: "Data 1",
+        return jwt.sign(payload, privateKEY, signOptions);
+    //console.log('Token: ' + token);
+    },
+    
+    verify: (token, $Options) => {
+    // JWT VERIFY
+    /*
+    vOptions = {
+        i = 'Mechspace'; //Org issuing token
+        s = 'me@mechspace.co'; //User of Token
+        a = 'https://mechspace.co/'; //Intended Recipient of Token
+    }
+    */
+        var verifyOptions = {
+            issuer: $Options.i,
+            subject: $Options.s,
+            audience: $Options.a,
+            expiresIn: "12h",
+            algorithms: ["RS256"]
+        };
+
+        return jwt.verify(token, publicKEY, verifyOptions);
+        //console.log(`\nJWT Verification result: ` + JSON.stringify(verified));
+    },
+    
+    decode: (token) => {
+        return jwt.decode(token, {complete: true});
+        // returns null if invalid
+    }
 };
-
-var i = 'Mechspace'; //Org issuing token
-var s = 'me@mechspace.co'; //User of Token
-var a = 'https://mechspace.co/'; //Intended Recipient of Token
-
-var signOptions = {
-    issuer: i,
-    subject: s,
-    audience: a,
-    expiresIn: "12h",
-    algorithm: "RS256"
-};
-
-var token = jwt.sign(payload, privateKEY, signOptions);
-console.log('Token: ' + token);
-
-// JWT VERIFY
-var verifyOptions = {
-    issuer: i,
-    subject: s,
-    audience: a,
-    expiresIn: "12h",
-    algorithms: ["RS256"]
-};
-
-var verified = jwt.verify(token, publicKEY, verifyOptions);
-console.log(`\nJWT Verification result: ` + JSON.stringify(verified));
-console.log(jwt.decode(token, {complete:true}));
 
 
